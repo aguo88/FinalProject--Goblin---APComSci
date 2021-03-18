941803d8afa047948a0b89347e228a3964bb1717
Andrew-Ghee-UO <47193626+Andrew-Ghee-UO@users.noreply.github.com>
2021-03-18 10:29:01 -0400

    pid setup

--- include/subsytemHeaders/PID.hpp
+++ include/subsytemHeaders/PID.hpp
@@ -1,5 +1,23 @@
 #include "main.h"
 
-//Basic PID (MOTOR)
-void PIDcount(int counts);
-void PIDdeg(int degrees);
+class PID
+{
+  private:
+  double p, i, d, error, target;
+
+  public:
+  double sum, last;
+  pros::Motor motor();
+  PID(double kP, double kI, double kD, double setPoint, pros::Motor motor);
+  double getP();
+  void setP(double p);
+  double getI();
+  void setI(double i);
+  double getD();
+  void setD(double d);
+  double getError();
+  void setError(double error);
+  double getTarget();
+  void setTarget(double target);
+  double PIDcount(int target);
+};
--- include/subsytemHeaders/constants.hpp
+++ include/subsytemHeaders/constants.hpp
@@ -7,3 +7,6 @@ extern const int RIGHT_DRIVE_PORT = 1;
 
 //MOTOR SCALE
 extern const int MOTOR_SCALE = 127;
+
+//Loop Delay
+extern const double ROBOT_DELAY = 0.01;
