9fd8df0b73ee4514effdd2299755b9f2f50b656d
Wyeeeth <wyethshiu@Wyeths-MacBook.local>
2021-03-18 12:29:44 -0400

    PID pull

--- include/subsytemHeaders/globals.hpp
+++ include/subsytemHeaders/globals.hpp
@@ -3,6 +3,8 @@
 //Motors
 extern pros::Motor leftDriveMotor;
 extern pros::Motor rightDriveMotor;
+extern pros::Motor armMotor;
+extern pros::Motor clawMotor
 
 //Controller
 extern pros::Controller controller;
--- src/subsystems/globals.cpp
+++ src/subsystems/globals.cpp
@@ -4,6 +4,7 @@
 //(port, gearset, reversed, encoder units)
 pros::Motor leftDriveMotor(LEFT_DRIVE_PORT, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor rightDriveMotor(RIGHT_DRIVE_PORT, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor armMotor(ARM_PORT, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
 
 //(Master for 1, partner for 2)
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
