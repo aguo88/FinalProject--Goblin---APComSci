02d31b0fc8c76076157db2c78ab0539d69d21213
Andrew-Ghee-UO <47193626+Andrew-Ghee-UO@users.noreply.github.com>
2021-03-18 13:58:20 -0400

    more arm changes

--- include/subsytemHeaders/arm.hpp
+++ include/subsytemHeaders/arm.hpp
@@ -1,7 +1,7 @@
 #include "main.h"
 
 void armcontrol();
-void manualControl(int direction);
-void autoControl(int preset);
+void autoControl(double power);
+void setArmDeg(int degrees);
 //std::int32_t pros::Controller::get_digital ( pros::controller_digital_e_t button )
 //std::int32_t pros::Motor::move_velocity ( const std::int32_t velocity )
--- src/subsystems/arm.cpp
+++ src/subsystems/arm.cpp
@@ -10,27 +10,31 @@ void autoControl(double power) {
   armMotor.move(power);
 }
 
+void setArmDeg(int degrees) {
+  armPID.setTarget(degrees);
+}
+
 //Always under PID loop
 void armControl() {
   //Manual & Preset Control
   if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_A) == 1 && armPID.getTarget() - 2 > 0) {
     //Move down (manual)
     if(armPID.getTarget() + maxArmDegMovement < 0)
-      armPID.setTarget(0);
+      setArmDeg(0);
     else
-      armPID.setTarget(armPID.getTarget() - maxArmDegMovement);
-  } else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_A) == 1 && armPID.getTarget() + maxArmDegMovement < 90) {
+      setArmDeg(armPID.getTarget() - maxArmDegMovement);
+  } else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_B) == 1 && armPID.getTarget() + maxArmDegMovement < 90) {
     //Move up (manual)
     if(armPID.getTarget() + maxArmDegMovement > 90)
-      armPID.setTarget(90);
+      setArmDeg(90);
     else
-      armPID.setTarget(armPID.getTarget() + maxArmDegMovement);
+      setArmDeg(armPID.getTarget() + maxArmDegMovement);
   } else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_X) == 1) {
     //Move to 90 deg (preset)
-    armPID.setTarget(90);
+    setArmDeg(90);
   } else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y) == 1) {
     //Move to 0 deg (preset)
-    armPID.setTarget(0);
+    setArmDeg(0);
   }
   autoControl(armPID.PIDcount());
 }
