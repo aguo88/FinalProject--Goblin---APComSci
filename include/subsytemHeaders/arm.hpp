c54cdd9737e4ca21d26b5e77c3d2d94fe3201c28
Wyeeeth <wyethshiu@Wyeths-MacBook.local>
2021-03-20 13:45:34 -0400

    claw class

---
+++ include/subsytemHeaders/arm.hpp
@@ -0,0 +1,7 @@
+#include "main.h"
+
+void armcontrol();
+void autoControl(double power);
+void setArmDeg(int degrees);
+//std::int32_t pros::Controller::get_digital ( pros::controller_digital_e_t button )
+//std::int32_t pros::Motor::move_velocity ( const std::int32_t velocity )
