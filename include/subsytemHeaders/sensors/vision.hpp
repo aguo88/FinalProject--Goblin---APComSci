cf717d282cc4d846bf1d28cf7b45fd7bb111f0a4
Andrew-Ghee-UO <47193626+Andrew-Ghee-UO@users.noreply.github.com>
2021-03-22 13:45:08 -0400

    VISION ICU

--- include/subsytemHeaders/sensors/vision.hpp
+++ include/subsytemHeaders/sensors/vision.hpp
@@ -1,3 +1,18 @@
 #include "main.h"
 
+int CURRENT_ID;
+
+pros::vision_object_s_t currentObj;
+
+//X & Y pos of object in cam FOV
+int currentTargetX;
+int currentTargetY;
+
 void setVision();
+void findObjects();
+double getArea();
+double getDistance();
+void clearVision();
+void checkExposure(int exposure);
+
+
--- src/subsystems/sensors/vision.cpp
+++ src/subsystems/sensors/vision.cpp
@@ -2,10 +2,54 @@
 #include "subsytemHeaders/globals.hpp"
 
 //config these when we get act camera
-pros::vision_signature_s_t RED_SIG = pros::Vision::signature_from_utility(0, 8973, 11143, 10058, -2119, -1053, -1586, 5.4, 0);
-pros::vision_signature_s_t GREEN_SIG = pros::Vision::signature_from_utility(1, 8973, 11143, 10058, -2119, -1053, -1586, 5.4, 0);
-pros::vision_signature_s_t BLUE_SIG = pros::Vision::signature_from_utility(2, 8973, 11143, 10058, -2119, -1053, -1586, 5.4, 0);
+const int RED_ID = 1;
+const int BLUE_ID = 2;
+const int GREEN_ID = 3;
+
+//minimum area of acceptable object
+const int MIN_SIZE = 5000;
+//maximum area of acceptable object
+const int MAX_SIZE = 6000;
+
+int CURRENT_ID = 0;
+
+pros::vision_object_s_t currentObj = visionCam.get_by_sig(0, CURRENT_ID);
+
+void checkExposure(int exposure) {
+  if(visionCam.get_exposure() > exposure) {
+    visionCam.set_exposure(exposure);
+  }
+}
+
+void findObject() {
+  currentObj = visionCam.get_by_sig(0, CURRENT_ID);
+}
+
+double getArea() {
+  return currentObj.width * currentObj.height;
+}
+
+double getAngle() {
+  //test both in degrees
+  return currentObj.angle / 10.0;
+  // return currentObj.x_middle_coord
+}
+
+int checkDistance() {
+  int area = getArea();
+  if(area > MAX_SIZE) {
+    //too close
+    return -1;
+  } else if(area <= MAX_SIZE && area >= MIN_SIZE) {
+    //just right
+    return 0;
+  } else {
+    //too far
+    return 1;
+  }
+}
 
 void setVision() {
-
+  checkExposure(50);
+  findObject();
 }
